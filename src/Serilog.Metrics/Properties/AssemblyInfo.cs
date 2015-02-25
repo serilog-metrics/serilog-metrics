22d77b5dfc31eca262cd259da9e252764d73334d
Michiel van Oudheusden <m.vanoudheusden@aerdata.com>
2015-02-25 22:51:07 +0100

    Moved over the Serilog.Extras.Timing to Serology.Metrics

---
+++ src/Serilog.Metrics/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Serilog.Metrics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2014")]
